9e09a6ebe792b705df14f27c9bc0bf8c02bffa5c
Yoav Karako <yoav@soluto.com>
2019-04-08 16:44:35 +0300

    dependency update

--- src/mongoDbProjection.ts
+++ src/mongoDbProjection.ts
@@ -10,7 +10,7 @@ export const getMongoDbProjection = logOnError((info: GraphQLResolveInfo, graphQ
     if (!Object.keys(info).includes('fieldNodes')) throw 'First argument of "getMongoDbProjection" must be a GraphQLResolveInfo';
     if (!isType(graphQLFieldsType)) throw 'Second argument of "getMongoDbProjection" must be a GraphQLType';
 
-    const nodes = flatten(info.fieldNodes.map(_ => _.selectionSet.selections));
+    const nodes = flatten(info.fieldNodes.map(_ => [..._.selectionSet.selections]));
 
     const projection = getSelectedProjection(nodes, graphQLFieldsType, { info, fragments: {} });
 
@@ -44,7 +44,7 @@ function getSelectedProjection(
                 [node.name.value]: 1
             };
 
-            const nested = getSelectedProjection(node.selectionSet.selections, getInnerType(field.type) as GraphQLFieldsType, extra);
+            const nested = getSelectedProjection([...node.selectionSet.selections], getInnerType(field.type) as GraphQLFieldsType, extra);
 
             return {
                 ...projection,
@@ -54,7 +54,7 @@ function getSelectedProjection(
             const type = extra.info.schema.getType(node.typeCondition.name.value);
             return {
                 ...projection,
-                ...getSelectedProjection(node.selectionSet.selections, type as GraphQLFieldsType, extra, ...excludedFields)
+                ...getSelectedProjection([...node.selectionSet.selections], type as GraphQLFieldsType, extra, ...excludedFields)
             };
         } else if (node.kind === 'FragmentSpread') {
             return {
@@ -72,7 +72,7 @@ function getFragmentProjection(
     const fragmentName = fragmentSpreadNode.name.value;
     if (extra.fragments[fragmentName]) return extra.fragments[fragmentName];
     const fragmentNode = extra.info.fragments[fragmentName];
-    extra.fragments[fragmentName] = getSelectedProjection(fragmentNode.selectionSet.selections, graphQLFieldsType, extra);
+    extra.fragments[fragmentName] = getSelectedProjection([...fragmentNode.selectionSet.selections], graphQLFieldsType, extra);
     return extra.fragments[fragmentName];
 }
 
