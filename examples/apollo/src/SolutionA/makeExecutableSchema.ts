52fea3e3d124e56d9d7a9730e69640996add579c
Ibby Hadeed <ibby@zyra.ca>
2019-08-13 17:17:20 -0400

    feat: handle all enum values

--- examples/apollo/src/SolutionA/makeExecutableSchema.ts
+++ examples/apollo/src/SolutionA/makeExecutableSchema.ts
@@ -1,23 +1,33 @@
-import { GraphQLSchema, GraphQLNamedType, GraphQLInputObjectType, GraphQLInputType, isInputObjectType, isNonNullType, isListType } from "graphql";
-import { makeExecutableSchema, gql, IExecutableSchemaDefinition } from "apollo-server-express";
-import { visitors, types as directiveTypes, MongoDirectivesContext } from "./directives";
-import { getTypesCache, clearTypesCache, GraphQLPaginationType, GraphQLSortType } from "graphql-to-mongodb";
-import { printType } from "graphql";
+import { gql, IExecutableSchemaDefinition, IResolvers, makeExecutableSchema } from "apollo-server-express";
+import {
+    GraphQLEnumValue,
+    GraphQLInputObjectType,
+    GraphQLInputType,
+    GraphQLNamedType,
+    GraphQLSchema,
+    isEnumType,
+    isInputObjectType,
+    isListType,
+    isNonNullType,
+    printType
+} from "graphql";
+import { clearTypesCache, getTypesCache, GraphQLPaginationType, GraphQLSortType } from "graphql-to-mongodb";
+import { MongoDirectivesContext, types as directiveTypes, visitors } from "./directives";
 
 export default function <TContext>(config: IExecutableSchemaDefinition<TContext>): GraphQLSchema {
     clearTypesCache();
     MongoDirectivesContext.stage = "First";
 
     const configTypeDefs = Array.isArray(config.typeDefs) ? config.typeDefs : [config.typeDefs];
-    
+
     makeExecutableSchema({
         ...config,
         typeDefs: [...configTypeDefs, directiveTypes],
         schemaDirectives: { ...config.schemaDirectives, ...visitors }
-    })
+    });
 
     let typesCache = getTypesCache();
-    resolveLazyFields(Object.keys(typesCache).map(_ => typesCache[_]).filter(isInputObjectType))
+    resolveLazyFields(Object.keys(typesCache).map(_ => typesCache[_]).filter(isInputObjectType));
     typesCache = getTypesCache();
     typesCache[GraphQLPaginationType.name] = GraphQLPaginationType;
     typesCache[GraphQLSortType.name] = GraphQLSortType;
@@ -27,24 +37,30 @@ export default function <TContext>(config: IExecutableSchemaDefinition<TContext>
         .join("\n");
 
     const typesSdl = gql(typesSdlRaw);
-    
-    // fetch OprExists as an Enum type
-    const oprExists = typesCache.OprExists as GraphQLEnumType;
 
-    // define a resolver for OprExists to return our custom values instead of enum key
-    config.resolvers['OprExists'] = oprExists.getValues().reduce((resolver, value) => {
-        resolver[value.name] = value.value;
-        return resolver;
-    }, {});
+    const cachedTypeKeys: string[] = Object.keys(typesCache);
+
+    for (const key of cachedTypeKeys) {
+        const enumType = typesCache[key];
+
+        if (!isEnumType(enumType)) {
+            continue;
+        }
+
+        config.resolvers[key] = enumType.getValues().reduce((resolver: IResolvers, value: GraphQLEnumValue) => {
+            resolver[value.name] = value.value;
+            return resolver;
+        }, {});
+    }
 
     MongoDirectivesContext.stage = "Second";
+
     const stageTwoSchema = makeExecutableSchema({
         ...config,
         typeDefs: [...configTypeDefs, typesSdl, directiveTypes],
         schemaDirectives: { ...config.schemaDirectives, ...visitors }
-    })
+    });
 
-    
     return stageTwoSchema;
 }
 
@@ -56,12 +72,13 @@ function resolveLazyFields(types: GraphQLInputObjectType[]) {
             .keys(fields)
             .map(key => innerType(fields[key].type))
             .filter(isInputObjectType)
-            .filter(_ => !typesCache[_.name]))
+            .filter(_ => !typesCache[_.name]));
     });
 }
 
 function innerType(type: GraphQLInputType): GraphQLInputType & GraphQLNamedType {
-    if (isNonNullType(type) || isListType(type)) 
+    if (isNonNullType(type) || isListType(type)) {
         return innerType(type.ofType);
+    }
     return type;
 }
